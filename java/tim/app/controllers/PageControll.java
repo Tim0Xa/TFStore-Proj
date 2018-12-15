ddb01ee2f32b200e7dc7a52d8bca61c7f673f3ad
Tim0Xa <kogtim@gmial.com>
2018-12-15 10:26:01 +0200

    Home page, Welcome page - are set Controller, + HTML

--- java/tim/app/controllers/PageControll.java
+++ java/tim/app/controllers/PageControll.java
@@ -1,4 +1,20 @@
 package tim.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PageControll {
+
+    @RequestMapping("/")
+    public String welcomePage(){
+        return "welcome.html";
+    }
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "home.html";
+    }
+
 }
