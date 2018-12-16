ccfd508c40b1ff288ae2471bb26f76198cdbd720
Tim0Xa <kogtim@gmial.com>
2018-12-16 07:18:59 +0200

    File driver, methods finished:    move    read    copy    write    delete    listFiles    listNames

--- java/tim/app/controllers/FileDriver.java
+++ java/tim/app/controllers/FileDriver.java
@@ -7,22 +7,35 @@ import java.util.ArrayList;
 
 public class FileDriver {
 
-    public void move(){};
-    public void read(){};
-    public void coppy(){};
+    public void move(String from, String to) throws IOException {
+        Files.move(new File(from).toPath(), new File(to).toPath());
+    }
+
+    public byte[] read(String adr) throws IOException {
+        return Files.readAllBytes(new File(adr).toPath());
+    }
+
+    public boolean copy(String from, String to) throws IOException {
+        return write(to, read(from));
+    }
+
     public boolean write(String adr, byte[] data) throws IOException {
-        return Files.write(new File(adr).toPath()
-                , data).toFile().isFile();};
+        return
+                Files.write(new File(adr).toPath()
+                , data).toFile().isFile();}
+
     public boolean delete(String adr){
         return new File(adr).delete();
-    };
+    }
+
     public String[] listNames(String adr){
-        ArrayList<String> res = new ArrayList<String>();
+        ArrayList<String> res = new ArrayList<>();
         for (File f : listFiles(adr) ) {
             res.add(f.getName());}
         return res.toArray(new String[0]);
-    };
+    }
+
     public File[] listFiles(String adr){
         return new File(adr).listFiles();
-    };
+    }
 }
