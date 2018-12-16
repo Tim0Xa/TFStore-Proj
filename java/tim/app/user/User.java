1d53d12ea86d0a4f4cd8188998f94ad79ccabb8b
Tim0Xa <kogtim@gmial.com>
2018-12-16 18:39:08 +0200

    Bean - User added adjusted:   home.html   welcome.html

---
+++ java/tim/app/user/User.java
@@ -0,0 +1,16 @@
+package tim.app.user;
+
+
+public class User {
+
+    String rootFolder;
+    String[] curFold;
+
+    long idUser;
+    String userName;
+    String userPassword;
+
+    String ROULE;
+
+
+}
