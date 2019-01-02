dc5b332c1c1fc4f149b6d056f113a1002f8371ee
Tim0Xa <kogtim@gmial.com>
2019-01-02 15:30:14 +0200

    home.html - adjusted welcome.html - small changes RestHandler.java - created for test of API Status: no Login, Api is listing files from default address, throw REST controller

---
+++ java/tim/app/controllers/RestHandler.java
@@ -0,0 +1,13 @@
+package tim.app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestHandler {
+
+    @GetMapping("/rest")
+    String[] listFolder(){
+        return new FileDriver().listNames("C:/Users/tim0f/Downloads/");
+    }
+}
