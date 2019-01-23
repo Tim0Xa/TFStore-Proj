b51d851215684825607a3d0217f384545fe3984c
Tim0Xa <kogtim@gmial.com>
2019-01-23 21:55:44 +0200

    User class reference added Addres line on the home page is working

--- java/tim/app/controllers/RestResponseController.java
+++ java/tim/app/controllers/RestResponseController.java
@@ -1,13 +1,24 @@
 package tim.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestResponseController {
 
-    @GetMapping("/rest")
-    String[] listFolder(){
-        return new FileDriver().listNames("C:/Users/tim0f/Downloads/");
+//    @GetMapping("/rest")
+//    String[] listFolder(){
+//        return new FileDriver().listNames("C:/Users/tim0f/Downloads/");
+//    }
+
+    @PostMapping("/rest")
+    String[] listFolder (@RequestBody String addr) {
+        addr = addr.replace("%3A",":")
+                .replace("%2F","/");
+        addr =  addr.substring(0, addr.length() - 1);
+        System.out.println(addr);
+        return new FileDriver().listNames(addr);
     }
 }
--- java/tim/app/repositories/UserRepo.java
+++ java/tim/app/repositories/UserRepo.java
@@ -1,6 +1,7 @@
 package tim.app.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import tim.app.user.User;
 
 public interface UserRepo extends CrudRepository<User, Long> {
 }
